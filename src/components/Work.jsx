08e285c541fd0b1c008dfe222ec557018b156f21
jonathanbridges <jbridges7@gmail.com>
2019-09-10 13:13:36 -0700

    Update Work Links

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -20,9 +20,8 @@ export default class Work extends React.Component {
                     <span>A data visualization of trending items on Twitter</span>
                     <span>d3, ExpressJS</span>
                     <p className="icon">
-                      <span><a href="#"><i className="icon-share3" /></a></span>
-                      <span><a href="https://twitter-geotrends.herokuapp.com" target="_blank" rel="noopener noreferrer"><i className="icon-eye" /> 100</a></span>
-                      <span><a href="#"><i className="icon-heart" /> 49</a></span>
+                      <span><a href="https://twitter-geotrends.herokuapp.com" target="_blank" rel="noopener noreferrer"><i className="icon-eye" /> Live</a></span>
+                      <span><a href="https://github.com/jonathanbridges/twitter-geotrends" target="_blank" rel="noopener noreferrer"><i className="icon-github" /> Repo</a></span>
                     </p>
                   </div>
                 </div>
@@ -32,13 +31,12 @@ export default class Work extends React.Component {
               <div className="project" style={{ backgroundImage: 'url(images/img-2.jpg)' }}>
                 <div className="desc">
                   <div className="con">
-                    <h3><a href="https://click-camp.herokuapp.com">ClickCamp</a></h3>
+                    <h3><a href="https://click-camp.herokuapp.com" target="blank" rel="noopener noreferrer">ClickCamp</a></h3>
                     <span>A single-page web app inspired by HipCamp</span>
                     <span>Ruby on Rails, ReactJS, Redux, PostgreSQL, GoogleMaps API, Amazon S3</span>
                     <p className="icon">
-                      <span><a href="#"><i className="icon-share3" /></a></span>
-                      <span><a href="#"><i className="icon-eye" /> 100</a></span>
-                      <span><a href="#"><i className="icon-heart" /> 49</a></span>
+                      <span><a href="https://click-camp.herokuapp.com" target="blank" rel="noopener noreferrer"><i className="icon-eye" /> Live</a></span>
+                      <span><a href="https://github.com/jonathanbridges/click-camp" target="blank" rel="noopener noreferrer"><i className="icon-github" /> Repo</a></span>
                     </p>
                   </div>
                 </div>
@@ -48,19 +46,18 @@ export default class Work extends React.Component {
               <div className="project" style={{ backgroundImage: 'url(images/img-3.jpg)' }}>
                 <div className="desc">
                   <div className="con">
-                    <h3><a href="https://spacey-aa.herokuapp.com">Spacey</a></h3>
+                    <h3><a href="https://spacey-aa.herokuapp.com" target="blank" rel="noopener noreferrer">Spacey</a></h3>
                     <span>A single-page website that aggregates space industry content</span>
                     <span>MongoDB, ExpressJS, ReactJS, Node</span>
                     <p className="icon">
-                      <span><a href="#"><i className="icon-share3" /></a></span>
-                      <span><a href="#"><i className="icon-eye" /> 100</a></span>
-                      <span><a href="#"><i className="icon-heart" /> 49</a></span>
+                      <span><a href="https://spacey-aa.herokuapp.com" target="blank" rel="noopener noreferrer"><i className="icon-eye" /> Live</a></span>
+                      <span><a href="https://github.com/jonathanbridges/spacey" target="blank" rel="noopener noreferrer"><i className="icon-heart" /> Repo</a></span>
                     </p>
                   </div>
                 </div>
               </div>
             </div>
-            <div className="col-md-6 animate-box" data-animate-effect="fadeInBottom">
+            {/* <div className="col-md-6 animate-box" data-animate-effect="fadeInBottom">
               <div className="project" style={{ backgroundImage: 'url(images/img-4.jpg)' }}>
                 <div className="desc">
                   <div className="con">
@@ -104,13 +101,13 @@ export default class Work extends React.Component {
                   </div>
                 </div>
               </div>
-            </div>
+            </div> */}
           </div>
-          <div className="row">
+          {/* <div className="row">
             <div className="col-md-12 animate-box">
               <p><a href="#" className="btn btn-primary btn-lg btn-load-more">Load more <i className="icon-reload" /></a></p>
             </div>
-          </div>
+          </div> */}
         </div>
       </section>
     )
