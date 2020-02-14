3a3d60d5c85e82e99ded88531c9da913b7d5de9c
jonathanbridges <jbridges7@gmail.com>
2020-02-14 09:21:06 -0800

    Update experience section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -17,18 +17,29 @@ export default class Experience extends React.Component {
                 <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
                   <div className="timeline-entry-inner">
                     <div className="timeline-icon color-1">
-                      <i className="icon-pen2" />
+                      <i className="icon-code" />
                     </div>
                     <div className="timeline-label">
-                      <h2>Software Developer at <a href="https://www.brandly.com" target="_blank" rel="noopener noreferrer">Brandly</a> <span>2019-present</span></h2>
-                      <p>At Brandly I have been refactoring CSS for mobile views across all marketing pages. I am also tackling bug fixes and frontend tests.</p>
+                      <h2>Software Engineer I at <a href="https://khoros.com" target="_blank" rel="noopener noreferrer">Khoros</a> <span>2020-present</span></h2>
+                      {/* <p></p> */}
+                    </div>
+                  </div>
+                </article>
+                <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                  <div className="timeline-entry-inner">
+                    <div className="timeline-icon color-2">
+                      <i className="icon-code" />
+                    </div>
+                    <div className="timeline-label">
+                      <h2>Software Developer at <a href="https://www.brandly.com" target="_blank" rel="noopener noreferrer">Brandly</a> <span>2019-2020</span></h2>
+                      <p>As a freelance developer for Brandly, I worked on new features like pagination on admin order histories, bug fixes, and mobile design.</p>
                     </div>
                   </div>
                 </article>
                 <article className="timeline-entry animate-box" data-animate-effect="fadeInRight">
                   <div className="timeline-entry-inner">
-                    <div className="timeline-icon color-2">
-                      <i className="icon-pen2" />
+                    <div className="timeline-icon color-3">
+                      <i className="icon-help" />
                     </div>
                     <div className="timeline-label">
                       <h2>Customer Support Technical Lead at <a href="https://www.tintup.com" target="_blank" rel="noopener noreferrer">TINT</a> <span>2015-2018</span></h2>
@@ -39,8 +50,8 @@ export default class Experience extends React.Component {
                 </article>
                 <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
                   <div className="timeline-entry-inner">
-                    <div className="timeline-icon color-3">
-                      <i className="icon-pen2" />
+                    <div className="timeline-icon color-4">
+                      <i className="icon-help" />
                     </div>
                     <div className="timeline-label">
                       <h2>Client Experience / Operations Associate at <a href="https://www.stitchfix.com" target="_blank" rel="noopener noreferrer">Stitch Fix</a> <span>2014-2015</span></h2>
@@ -52,8 +63,8 @@ export default class Experience extends React.Component {
                 </article>
                 <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
                   <div className="timeline-entry-inner">
-                    <div className="timeline-icon color-4">
-                      <i className="icon-pen2" />
+                    <div className="timeline-icon color-5">
+                      <i className="icon-briefcase3" />
                     </div>
                     <div className="timeline-label">
                       <h2>Purchasing Specialist at <a href="https://www.firstrepublic.com" target="_blank" rel="noopener noreferrer">First Republic Bank</a> <span>2014-2015</span></h2>
@@ -63,8 +74,8 @@ export default class Experience extends React.Component {
                 </article>
                 <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
                   <div className="timeline-entry-inner">
-                    <div className="timeline-icon color-5">
-                      <i className="icon-pen2" />
+                    <div className="timeline-icon color-1">
+                      <i className="icon-microphone" />
                     </div>
                     <div className="timeline-label">
                       <h2>Editor / Audio Engineer <a href="https://digifx.com" target="_blank" rel="noopener noreferrer">DigiFX</a> <span>2009-2010</span></h2>
@@ -74,8 +85,8 @@ export default class Experience extends React.Component {
                 </article>
                 <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
                   <div className="timeline-entry-inner">
-                    <div className="timeline-icon color-6">
-                      <i className="icon-pen2" />
+                    <div className="timeline-icon color-2">
+                      <i className="icon-microphone" />
                     </div>
                     <div className="timeline-label">
                       <h2>Studio Assistant <a href="http://soundtrackgroup.com/" target="_blank" rel="noopener noreferrer">Soundtrack Boston</a> <span>2008-2009</span></h2>
